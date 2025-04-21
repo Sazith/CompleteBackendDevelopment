cd588fd7c2fe6176ae80b72f7a17fd3a4d987e9d
Sazith <sazith.simecsoftdev@gmail.com>
2025-04-21 20:38:01 +0600

    sending response

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-  console.log(req);
+  console.log(req.url, req.headers, req.method);
+  res.setHeader('Content-type','text/html')
+  res.write('<html>')
+  res.write('<head><title>Complete Backend Development</title></head>')
+  res.write('<body><h1>Learning node js</h1></body>')
+  res.write('</html>')
+  res.end();
   
 })
 
