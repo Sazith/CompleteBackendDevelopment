5d98b7f31b201aeca1b3a0d383789c872969bf24
Sazith <sazith.simecsoftdev@gmail.com>
2025-04-24 17:38:10 +0600

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userRequestHandler =  require('./user')
 
 const server = http.createServer(userRequestHandler)
 
-const PORT = 8000;
+const PORT = 8002;
 server.listen(PORT, ()=>{
   console.log(`listening to the server at port ${PORT}`);
   
