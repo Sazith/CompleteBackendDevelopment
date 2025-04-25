e61c2dfcd77482b82c655aedd8f818ccd8a447f1
Sazith <sazith.simecsoftdev@gmail.com>
2025-04-25 11:59:40 +0600

    express router

---
+++ Airbnb/app.js
@@ -0,0 +1,51 @@
+// External Module
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Local Module
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+
+const app = express()
+
+app.use((req, res, next) =>{
+  console.log(req.url, req.method);
+  next()
+})
+
+app.use(express.urlencoded());
+
+app.use(userRouter);
+
+// app.get("/",(req, res, next) => {
+//   res.send(`
+//     <h1>Welcome to Airbnb</h1>
+//     <a href="/host/add-home">Add Home</a>
+//     `);
+// })
+
+app.use(hostRouter);
+// app.get("/host/add-home",(req, res, next) => {
+//   res.send(`
+//     <h1>Register your home here</h1>
+//     <form action="/host/add-home" method="POST">
+//     <input type="text" name="houseName" placeholder="Enter your house name"/>
+//     <input type="submit" value="Submit"/>
+//     </form>
+//     `);
+// })
+
+// app.post("/host/add-home",(req, res, next) => {
+//   console.log(req.body);
+//   res.send(`
+//     <h1>Home Registered Successfully</h1>
+//      <a href="/">Go to Home</a>
+//     `);
+// })
+
+
+const PORT = 8000;
+app.listen(PORT, ()=>{
+  console.log(`listening to the server at port ${PORT}`);
+  
+})
---
+++ Airbnb/routes/hostRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const hostRouter = express.Router();
+
+
+hostRouter.get("/host/add-home",(req, res, next) => {
+  res.send(`
+    <h1>Register your home here</h1>
+    <form action="/host/add-home" method="POST">
+    <input type="text" name="houseName" placeholder="Enter your house name"/>
+    <input type="submit" value="Submit"/>
+    </form>
+    `);
+})
+
+hostRouter.post("/host/add-home",(req, res, next) => {
+  console.log(req.body);
+  res.send(`
+    <h1>Home Registered Successfully</h1>
+     <a href="/">Go to Home</a>
+    `);
+})
+
+
+module.exports = hostRouter;
