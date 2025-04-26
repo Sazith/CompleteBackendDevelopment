cd670d5247559b43c475117afccd8a19b04723d1
Sazith <sazith.simecsoftdev@gmail.com>
2025-04-26 15:38:25 +0600

    controller added

--- Airbnb/routes/hostRouter.js
+++ Airbnb/routes/hostRouter.js
@@ -7,31 +7,12 @@ const hostRouter = express.Router();
 
 // Local Module
 const rootDir = require('../utils/pathUtil')
+const homeController = require('../controllers/home')
 
 const registerHome = []
-hostRouter.get("/add-home",(req, res, next) => {
-  // res.send(`
-  //   <h1>Register your home here</h1>
-  //   <form action="/host/add-home" method="POST">
-  //   <input type="text" name="houseName" placeholder="Enter your house name"/>
-  //   <input type="submit" value="Submit"/>
-  //   </form>
-  //   `);
-  // res.sendFile(path.join(__dirname,'../','views','addHome.html'))
-  // res.sendFile(path.join(__dirname,'../','views','addHome.html'))
-  res.sendFile(path.join(rootDir,'views','addHome.html'))
-})
+hostRouter.get("/add-home",homeController.getAddHome)
 
-hostRouter.post("/add-home",(req, res, next) => {
-  console.log(req.body);
-  // res.send(`
-  //   <h1>Home Registered Successfully</h1>
-  //    <a href="/">Go to Home</a>
-  //   `);
-  // res.sendFile(path.join(__dirname,'../','views','homeAdded.html'))
-   registerHome.push({houseName : req.body.houseName})
-  res.sendFile(path.join(rootDir,'views','homeAdded.html'))
-})
+hostRouter.post("/add-home",homeController.postAddHome)
 
 
 exports.hostRouter = hostRouter;
